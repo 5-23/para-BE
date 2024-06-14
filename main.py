57ec49c0250c7d216607b99d0cf204e6666aa438
Asta(아스타) <86705803+5-23@users.noreply.github.com>
2024-06-14 14:26:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from dotenv import load_dotenv
+from typing import Annotated
 load_dotenv()
 
 app = FastAPI()
@@ -13,6 +14,90 @@ app.mount("/public", StaticFiles(directory="public"), name="public")
 @app.get("/", response_class=HTMLResponse)
 def read_root():
     return FileResponse("./public/index.html")  
+
+@app.get("/post")
+def get_post(
+    page: Annotated[int, Header()] = 0
+):
+    posts = [
+        {
+            "author": "astara",
+            "title": "nou1",
+            "content": "str",
+            "id": "1"
+        },
+        {
+            "author": "asta",
+            "title": "nou2",
+            "content": "str",
+            "id": "2"
+        },
+        {
+            "author": "asta",
+            "title": "nou3",
+            "content": "str",
+            "id": "3"
+        },
+        {
+            "author": "asta",
+            "title": "nou4",
+            "content": "str",
+            "id": "4"
+        },
+        {
+            "author": "asta",
+            "title": "nou5",
+            "content": "str",
+            "id": "5"
+        },
+
+        {
+            "author": "asta",
+            "title": "nou6",
+            "content": "str",
+            "id": "6"
+        },
+
+        {
+            "author": "asta",
+            "title": "nou7",
+            "content": "str",
+            "id": "7"
+        },
+
+        {
+            "author": "asta",
+            "title": "nou8",
+            "content": "str",
+            "id": "8"
+        },
+    ]
+    if page*5+1 > posts.__len__():
+        return {
+            "status": 400,
+            "message": "no more posts"
+        }
+    try:
+        if (page+1)*5+1 > posts.__len__():
+            return {
+                "status": 200,
+                "posts": posts[page*5:page*5+5],
+                "message": "success",
+                "next": None
+            }
+        return {
+            "status": 200,
+            "posts": posts[page*5:page*5+5],
+            "message": "success",
+            "next": page+1
+        }
+    except:
+        return {
+            "status": 200,
+            "posts": posts[page*5:],
+            "message": "success",
+            "next": None
+        }
 
 """
 /signup
@@ -85,9 +170,9 @@ response: {
     status: int,
     posts: [
         {
+            author: str,
             title: str,
             content: str,
-            writer: str,
             id: str
         }, ...
     ],
